8ae8ca88d048b7eab0ecce64907eea7ef6428411
Joseph DiPeri <jdiperi88@gmail.com>
2017-09-20 16:53:26 -0400

    edited stocks some code and comments

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -6,19 +6,18 @@ init_stock_lookup = function() {
          init_stock_lookup();
     })
 }
+    //couldnt get errors to work
+    // $('#stock-lookupform').on('ajax:error',function(event, xhr, status, error){
+    //     $('#stock-lookup-results').replaceWith(' ');
+    //     $('#stock-lookup-errors').replaceWith('Stock was not found.')
+    // });
+    // was going to use a lifecycle method to render a loading..
+    // $('#stock-lookup-form').on('ajax:before', function(event, data, status){
+        
+    //     });
+        
 
-    $('#stock-lookupform').on('ajax:error',function(event, xhr, status, error){
-        $('#stock-lookup-results').replaceWith(' ');
-        $('#stock-lookup-errors').replaceWith('Stock was not found.')
-    });
-
-    $('#stock-lookup-form').on('ajax:before', function(event, data, status){
-        
-        });
-        
-        $('#stock-lookup-form').on('ajax:after', function(event, data, status){
-        
-        });
+    
 
 $(document).ready( function(){
     init_stock_lookup();
